4c44be781994a2e761ad071db23d8821c1790ae9
Nolan Knill <nolanknill@gmail.com>
2022-09-02 11:34:19 -0400

    Add comments to example in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useMemo } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // Define max mobile width
+  // Only create the mediaQuery object on mount. Memoize it so it doesn't run on every render
+  const mediaQuery = useMemo(() => window.matchMedia("(max-width: 767px)"), []);
+  
+  // is the current size mobile or not (767px)
+  const [isMobile, setIsMobile] = useState(mediaQuery.matches);
+  
+  // change handler changes isMobile state. Triggered when the window "match" changes 
+  const changeHandler = (event) => { setIsMobile(event.matches) };
+
+  useEffect(() => {
+    console.log("Added: Event listener for window change");
+    
+    // Triggered only when width changes threshold (max-width:767px)
+    mediaQuery.addEventListener("change", changeHandler);
+
+    return () => {
+      // Cleanup the event listener when this component unmounts
+      console.log("Removed: Event listener for window change");
+      mediaQuery.removeEventListener("change", changeHandler);
+    }
+  }, 
+  [mediaQuery]
   );
+
+  console.log("Rendering");
+
+  return (
+    <>
+      <h1>{isMobile ? "true" : "false"}</h1>
+    </>
+  )
 }
 
 export default App;
